be67e5521982d6f9e584e555b88a93af473f3b1a
Shaun Hubbard <shaunhubbard2013@icloud.com>
2022-06-28 16:36:03 -0400

    Initial Commit

---
+++ SwiftUI Quick Example/SwiftUI_Quick_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Quick_ExampleApp.swift
+//  SwiftUI Quick Example
+//
+//  Created by shaun on 6/28/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Quick_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
